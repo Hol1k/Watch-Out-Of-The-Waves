5a80b71dac41a7e2a55dafe5c4cc4d2954a7cfdb
Hol1k <danilugryumov96@gmail.com>
2025-02-17 18:33:55 +0500

    Moved OverlapSphere method form LootableItem to ItemLooter

--- Assets/Inventory/Items/ItemLooter.cs
+++ Assets/Inventory/Items/ItemLooter.cs
@@ -5,20 +5,43 @@ namespace Inventory.Items
     public class ItemLooter : MonoBehaviour
     {
         private EntityInventory _inventory;
+        
+        [SerializeField] private float lootRadius = 4f;
 
         private void Awake()
         {
             if (!TryGetComponent(out _inventory))
                 Debug.LogError("No component of type EntityInventory");
         }
-        
+
+        private void Update()
+        {
+            TargetLoot();
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.TryGetComponent(out LootableItem item))
             {
                 _inventory.AddItem(item.itemName, item.count);
                 item.GetComponent<SphereCollider>().enabled = false; // Need for fix double loot item
+                StopCoroutine(item.MovingCoroutine); //Need for fix using coroutine when item == null
                 Destroy(item.gameObject);
+            }
+        }
+
+        private void TargetLoot()
+        {
+            // ReSharper disable once Unity.PreferNonAllocApi
+            var hitColliders = Physics.OverlapSphere(transform.position, lootRadius);
+
+            foreach (Collider targetCollider in hitColliders)
+            {
+                if (targetCollider.TryGetComponent(out LootableItem target) && !target.isMovingToTarget)
+                {
+                    target.MovingCoroutine = StartCoroutine(target.MoveToTarget(this));
+                    target.isMovingToTarget = true;
+                }
             }
         }
     }
--- Assets/Inventory/Items/LootableItem.cs
+++ Assets/Inventory/Items/LootableItem.cs
@@ -7,10 +7,11 @@ namespace Inventory.Items
     {
         private Rigidbody _rb;
         
-        private bool _isMovingToTarget;
         private Collider[] _hitColliders;
-        [SerializeField] private float lootRadius = 4f;
         [SerializeField] private float lootForce = 5f;
+        
+        public bool isMovingToTarget;
+        public Coroutine MovingCoroutine;
         
         public string itemName;
         [Min(1)] public int count = 1;
@@ -20,13 +21,9 @@ namespace Inventory.Items
             _rb = GetComponent<Rigidbody>();
         }
 
-        private void Update()
+        public IEnumerator MoveToTarget(ItemLooter target)
         {
-            TargetPlayer();
-        }
-
-        private IEnumerator MoveToItemLooter(ItemLooter target)
-        {
+            _rb.useGravity = false;
             while (true)
             {
                 var direction = (target.transform.position - transform.position).normalized;
@@ -34,25 +31,6 @@ namespace Inventory.Items
                 yield return null;
             }
             // ReSharper disable once IteratorNeverReturns
-        }
-
-        private void TargetPlayer()
-        {
-            if (!_isMovingToTarget)
-            {
-                // ReSharper disable once Unity.PreferNonAllocApi
-                _hitColliders = Physics.OverlapSphere(transform.position, lootRadius);
-
-                foreach (Collider targetCollider in _hitColliders)
-                {
-                    if (targetCollider.TryGetComponent(out ItemLooter target))
-                    {
-                        _rb.useGravity = false;
-                        StartCoroutine(MoveToItemLooter(target));
-                        _isMovingToTarget = true;
-                    }
-                }
-            }
         }
     }
 }
